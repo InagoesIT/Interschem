a5556e1f081b906b8d8347c48305a505ef5f1bd3
InagoesIT <wenulife@gmail.com>
2020-12-12 19:13:27 +0100

    changed to graphics.h

---
+++ Interschem/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <graphics.h>
+#include <winbgim.h>
+#include <cmath>
+
+int main()
+{
+  initwindow(800,600);
+  circle(100, 50, 40);
+  getch();
+  closegraph();
+  return 0;
+}
